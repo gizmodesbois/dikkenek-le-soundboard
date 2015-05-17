ae853ea509dbb99ddef68d481749e8bc534e2cee
Kevin Vanzandberghe <k.vanzandberghe@gmail.com>
2015-05-17 23:53:22 +0200

    Initial commit.

---
+++ public/js/philippe.js
@@ -0,0 +1,25 @@
+$(document).on('keydown', function (e) {
+  if (e.keyCode == 80) {        // P
+    var file = 'ogg/philippe.ogg'
+  } else if (e.keyCode == 74) {// J
+    var file = 'ogg/je-sais.ogg'
+  } else if (e.keyCode == 86) { // V
+    var file = 'ogg/viens-ici.ogg'
+  } else if (e.keyCode == 84) { // T
+    var file = 'ogg/ta-gueule.ogg'
+  } else if (e.keyCode == 69) { // E
+    var file = 'ogg/sale-encule.ogg'
+  } else if (e.keyCode == 83) { // S
+    var file = 'ogg/salaud.ogg'
+  } else if (e.keyCode == 89) { // Y
+    var file = 'ogg/vas-y.ogg'
+  } else if (e.keyCode == 67) { // C
+    var file = 'ogg/petit-con.ogg'
+  } else if (e.keyCode == 77) { // M
+    var file = 'ogg/mourir-connard.ogg'
+  } else if (e.keyCode == 70) { // F
+    var file = 'ogg/mon-frere.ogg'
+  }
+  var audio = new Audio(file);
+  audio.play();
+});
