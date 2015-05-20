9a66912d629a791cb2e1459208a241f722a7c44b
Kevin Vanzandberghe <k.vanzandberghe@gmail.com>
2015-05-20 15:30:55 +0200

    Add 7 new sounds.

--- public/js/philippe.js
+++ public/js/philippe.js
@@ -16,8 +16,18 @@ $(document).on('keydown', function (e) {
     var file = fileFormat + '/ta-gueule.' + fileFormat;
   } else if (e.keyCode == 69) { // E
     var file = fileFormat + '/sale-encule.' + fileFormat;
+  } else if (e.keyCode == 81) { // Q
+    var file = fileFormat + '/qui-a-tue.' + fileFormat;
+  } else if (e.keyCode == 82) { // R
+    var file = fileFormat + '/rien-a-foutre.' + fileFormat;
   } else if (e.keyCode == 83) { // S
     var file = fileFormat + '/salaud.' + fileFormat;
+  } else if (e.keyCode == 65) { // A
+    var file = fileFormat + '/a-ce-jour.' + fileFormat;
+  } else if (e.keyCode == 66) { // B
+    var file = fileFormat + '/veux-ce-connard.' + fileFormat;
+  } else if (e.keyCode == 68) { // D
+    var file = fileFormat + '/vengeance.' + fileFormat;
   } else if (e.keyCode == 89) { // Y
     var file = fileFormat + '/vas-y.' + fileFormat;
   } else if (e.keyCode == 67) { // C
@@ -26,6 +36,10 @@ $(document).on('keydown', function (e) {
     var file = fileFormat + '/mourir-connard.' + fileFormat;
   } else if (e.keyCode == 70) { // F
     var file = fileFormat + '/mon-frere.' + fileFormat;
+  } else if (e.keyCode == 71) { // G
+    var file = fileFormat + '/african-americany.' + fileFormat;
+  } else if (e.keyCode == 72) { // H
+    var file = fileFormat + '/payer.' + fileFormat;
   }
   var audio = new Audio(file);
   audio.play();
